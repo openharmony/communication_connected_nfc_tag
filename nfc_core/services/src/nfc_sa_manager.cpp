1466f509c19e30ff40e283d2ec66ecec801c3fe5
wangdapeng <wangdapeng@kaihong.com>
2022-08-23 04:02:00 -0700

    when foundation process crashed add  re-registration broadcast process
    Signed-off-by: wangdapeng <wangdapeng@kaihong.com>

--- nfc_core/services/src/nfc_sa_manager.cpp
+++ nfc_core/services/src/nfc_sa_manager.cpp
@@ -72,7 +72,7 @@ void NfcSaManager::OnAddSystemAbility(int32_t systemAbilityId, const std::string
 {
     InfoLog("OnAddSystemAbility systemAbilityId:%{public}d added!", systemAbilityId);
     if (systemAbilityId != COMMON_EVENT_SERVICE_ID) {
-        InfoLog("ImCommonEventManager::OnAddSystemAbility systemAbilityId is not COMMON_EVENT_SERVICE_ID");
+        InfoLog("OnAddSystemAbility systemAbilityId is not COMMON_EVENT_SERVICE_ID");
         return;
     }
     nfcService_->eventHandler_->SubscribePackageChangedEvent();
